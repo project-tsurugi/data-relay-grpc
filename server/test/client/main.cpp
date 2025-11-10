cdc3e36de31965c32e8534a634fafa4f068bde02
t-horikawa <horikawa@nautilus-technologies.com>
2025-11-10 17:45:31 +0900

    enhance test client

--- server/test/client/main.cpp
+++ server/test/client/main.cpp
@@ -1,20 +1,59 @@
 #include <iostream>
 #include <stdexcept>
+#include <csignal>
+
 #include <gflags/gflags.h>
 
 #include "client.h"
 #include "session.h"
 
 DEFINE_string(dbname, "tsurugi", "the database name");
+DEFINE_int32(threads, 0, "the number of test threads");
+DEFINE_bool(dispose, true, "dispose the put blob file at the end of the session");
+DEFINE_bool(loop, false, "loop the put operation forever");
+
+static std::vector<std::thread> threads{};
+
+static void signal_handler([[maybe_unused]] int sig) {
+    FLAGS_loop = false;
+}
+
+void put() {
+    while (true) {
+        tateyama::session session{};
+        data_relay_grpc::blob_relay::Client client("localhost:50051", session.session_id());
+
+        client.put();
+        if (FLAGS_loop) {
+            continue;
+        }
+        break;
+    }
+}
 
 int main(int argc, char** argv) {
     try {
         gflags::SetUsageMessage("tateyama gRPC relay test program");
         gflags::ParseCommandLineNonHelpFlags(&argc, &argv, true);
         
-        tateyama::session session{};
-        data_relay_grpc::blob_relay::Client client("localhost:50051", session.session_id());
-        client.put();
+        if (signal(SIGINT, signal_handler) == SIG_ERR) {  // NOLINT  #define SIG_ERR  ((__sighandler_t) -1) in a system header file
+            std::cerr << "cannot register signal handler" << std::endl;
+            exit(1);
+        }
+
+        if (FLAGS_threads > 1) {
+            threads.reserve(FLAGS_threads);
+            for (int i = 0; i < FLAGS_threads; i++) {
+                threads.emplace_back(std::thread([&](){ put(); }));
+            }
+            for (std::thread &th : threads) {
+                if (th.joinable()) {
+                    th.join();
+                }
+            }
+        } else {
+            put();
+        }
         return 0;
     } catch (std::runtime_error &ex) {
         std::cerr << ex.what() << std::endl;
--- server/test/client/session.h
+++ server/test/client/session.h
@@ -2,16 +2,33 @@
 
 #include <cstdint>
 #include <memory>
+#include <thread>
+#include <chrono>
+#include <stdexcept>
+#include <gflags/gflags.h>
+
 #include "tateyama/transport/transport.h"
 #include "tateyama/proto/debug/request.pb.h"
 #include "tateyama/proto/debug/response.pb.h"
+
+DECLARE_bool(dispose);
 
 namespace tateyama {
 
 class session {
 public:
     session() {
-        transport_ = std::make_unique<tateyama::bootstrap::wire::transport>(tateyama::bootstrap::wire::transport::service_id_debug);
+        while (true) {
+            try {
+                transport_ = std::make_unique<tateyama::bootstrap::wire::transport>(tateyama::bootstrap::wire::transport::service_id_debug);
+                return;
+            } catch (std::runtime_error &ex) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                continue;
+            } catch (std::exception &ex) {
+                throw std::runtime_error(std::string("originally: ") + ex.what());
+            }
+        }
     }
     ~session() {
         dispose();
@@ -39,24 +56,25 @@ public:
         return -1;
     }
     void dispose() const noexcept {
-        try {
-            ::tateyama::proto::debug::request::Request request{};
-            (request.mutable_blob_relay_dispose_session())->set_session_id(session_id_);
-            auto response_opt = transport_->send<::tateyama::proto::debug::response::BlobRelayDisposeSession>(request);
+        if (FLAGS_dispose) {
+            try {
+                ::tateyama::proto::debug::request::Request request{};
+                (request.mutable_blob_relay_dispose_session())->set_session_id(session_id_);
+                auto response_opt = transport_->send<::tateyama::proto::debug::response::BlobRelayDisposeSession>(request);
 
-            if (response_opt) {
-                const auto& response = response_opt.value();
-                switch(response.result_case()) {
-                case ::tateyama::proto::debug::response::BlobRelayDisposeSession::ResultCase::kSuccess:
-                    return;
-                default:
-                    throw std::runtime_error("server error");
+                if (response_opt) {
+                    const auto& response = response_opt.value();
+                    switch(response.result_case()) {
+                    case ::tateyama::proto::debug::response::BlobRelayDisposeSession::ResultCase::kSuccess:
+                        return;
+                    default:
+                        throw std::runtime_error("server error");
+                    }
                 }
+            } catch (std::runtime_error &ex) {
+                std::cerr << ex.what() << std::endl;
             }
-        } catch (std::runtime_error &ex) {
-            std::cerr << ex.what() << std::endl;
         }
-        return;
     }
 
 private:
